3f6e84f954a619e8004bc037d813227c2d793e47
OhWellNevermind <52574266+OhWellNevermind@users.noreply.github.com>
2023-11-11 01:15:35 +0200

    init commit

--- models/contacts.js
+++ models/contacts.js
@@ -1,19 +1,71 @@
-// const fs = require('fs/promises')
+import fs from "fs/promises";
+import { nanoid } from "nanoid";
+import path, { resolve } from "path";
 
-const listContacts = async () => {}
+const contactsPath = resolve("models", "contacts.json");
 
-const getContactById = async (contactId) => {}
+const getContactIndex = (contacts, contactId) =>
+  contacts.findIndex((contact) => contact.id === contactId);
 
-const removeContact = async (contactId) => {}
+const updateContacts = (contacts) =>
+  fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
 
-const addContact = async (body) => {}
+export function listContacts() {
+  return fs
+    .readFile(contactsPath)
+    .then((contacts) => {
+      return JSON.parse(contacts);
+    })
+    .catch((error) => {
+      console.log(error.message);
+    });
+}
 
-const updateContact = async (contactId, body) => {}
+export function getContactById(contactId) {
+  return fs
+    .readFile(contactsPath)
+    .then((contactsBuffer) => {
+      const contacts = JSON.parse(contactsBuffer);
+      const index = getContactIndex(contacts, contactId);
+      if (index === -1) {
+        return null;
+      }
+      return contacts[index];
+    })
+    .catch((error) => {
+      console.log(error.message);
+    });
+}
 
-module.exports = {
-  listContacts,
-  getContactById,
-  removeContact,
-  addContact,
-  updateContact,
+export function removeContact(contactId) {
+  return fs
+    .readFile(contactsPath)
+    .then((contactsBuffer) => {
+      const contacts = JSON.parse(contactsBuffer);
+      const index = getContactIndex(contacts, contactId);
+      if (index === -1) {
+        return null;
+      }
+      const [result] = contacts.splice(index, 1);
+      updateContacts(contacts);
+      return result;
+    })
+    .catch((error) => {
+      console.log(error.message);
+    });
+}
+
+export function addContact(name, email, phone) {
+  return fs
+    .readFile(contactsPath)
+    .then((contactsBuffer) => {
+      const contacts = JSON.parse(contactsBuffer);
+      const newContact = { id: nanoid(), name, email, phone };
+      contacts.push(newContact);
+      updateContacts(contacts);
+      return newContact;
+    })
+    .catch((error) => {
+      console.log(error.message);
+    });
 }
